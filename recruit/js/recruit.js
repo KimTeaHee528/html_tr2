dfb4632a7961e4c5c289823e53e74e0344fd78f8
kth <spmm528@gmail.com>
2022-07-06 09:58:46 +0900

    a

--- recruit/js/recruit.js
+++ recruit/js/recruit.js
@@ -1,3 +1,10 @@
 function pop(){
 	window.open("pop.html","확인창","width=260,height=100");
 }
+function selectAll(selectAll)  {
+	const checkboxes 
+		= document.querySelectorAll('input[type="checkbox"]');
+	checkboxes.forEach((checkbox) => {
+	checkbox.checked = selectAll.checked
+	})
+}
