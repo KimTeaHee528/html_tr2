bc7323ad6fcfdfe70165d56e62ff6da3b36f3ca1
kth <spmm528@gmail.com>
2022-07-06 11:54:28 +0900

    ㅁ

--- recruit/js/recruit.js
+++ recruit/js/recruit.js
@@ -8,3 +8,10 @@ function selectAll(selectAll)  {
 	checkbox.checked = selectAll.checked
 	})
 }
+$("#btnm").click(function(){
+	if(confirm("정말 등록하시겠습니까 ?") == true){
+		alert("등록되었습니다");
+    }else{
+		return ;
+	}
+})
